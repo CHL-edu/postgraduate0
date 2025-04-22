75bb8d2b7e3e2cb38065944e4e50a033740cabe3
HLChen <hanleichen.work@outlook.com>
2025-04-22 15:10:12 +0800

    Create test Launch Fine_turning.py

---
+++ AUTO Content Contral/SecondTry/test Launch Fine_turning.py
@@ -0,0 +1,16 @@
+#test Launch Fine_turning
+from transformers import BertTokenizerFast
+import os
+# 加载预训练分词器
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-chinese")
+
+# 准备领域词汇（手动或从语料提取）
+new_tokens = ["新冠病毒", "核磁共振", "手术切除"]  # 示例词汇
+
+# 添加新词汇
+tokenizer.add_tokens(new_tokens)
+
+# 保存分词器
+SAVE_PATH = '/content/drive/MyDrive/Colab Notebooks/BERT/Fine_turning/Tokenizer'
+os.makedirs(SAVE_PATH, exist_ok=True)
+tokenizer.save_pretrained(SAVE_PATH)
